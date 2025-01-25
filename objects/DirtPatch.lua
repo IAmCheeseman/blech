507acc55d63ba32e5ad064876cbab45a120cfa67
Xander Swan <xanderswan32@gmail.com>
2025-01-24 22:26:45 -0500

    Improve ground decal drawing code

--- objects/DirtPatch.lua
+++ objects/DirtPatch.lua
@@ -7,31 +7,24 @@ function DirtPatch:new(x, y)
   self.sort = "floor"
 
   local s = 64/2
-  self.vertices = {
-    -s, -s,
-     s, -s,
-     s, s,
-    -s, s,
-  }
-  self.mesh = lg.newMesh(4, "fan", "dynamic")
+  self.mesh = lg.newMesh({
+    {-s, -s, 0, 0, 1, 1, 1, 1},
+    { s, -s, 1, 0, 1, 1, 1, 1},
+    { s,  s, 1, 1, 1, 1, 1, 1},
+    {-s,  s, 0, 1, 1, 1, 1, 1},
+  }, "fan", "static")
+
   self.mesh:setTexture(lg.newImage("assets/dirt_patch.png"))
 end
 
-function DirtPatch:getXy(pos)
-  local index = pos * 2 - 1
-  return cam:p3d(self.vertices[index], self.vertices[index + 1])
-end
-
 function DirtPatch:draw()
-  local tlx, tly = self:getXy(1)
-  self.mesh:setVertex(1, tlx, tly, 0, 0)
-  local trx, try = self:getXy(2)
-  self.mesh:setVertex(2, trx, try, 1, 0)
-  local brx, bry = self:getXy(3)
-  self.mesh:setVertex(3, brx, bry, 1, 1)
-  local blx, bly = self:getXy(4)
-  self.mesh:setVertex(4, blx, bly, 0, 1)
+  lg.push()
+  lg.scale(1, 0.5)
+  lg.rotate(-cam.r)
 
   lg.setColor(1, 1, 1)
   lg.draw(self.mesh, 0, 0)
+
+  lg.pop()
 end
+
